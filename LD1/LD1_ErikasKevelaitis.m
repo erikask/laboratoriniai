36145a5ae3ec037c3b7e2f7cf7fda40ff8a3fcba
Erikas Kėvelaitis <erikas.kev@gmail.com>
2026-09-11 10:46:39 +0300

    privaloma uzd

--- LD1/LD1_ErikasKevelaitis.m
+++ LD1/LD1_ErikasKevelaitis.m
@@ -1,3 +1,15 @@
 %
 % Erikas Kėvelaitis EF-25/2 2026-09-11
 %
+
+%
+%Paprastas skriptas
+%
+
+x = 1:32;
+y = x.^2;
+
+plot(x, y, 'o-r', x, y/3, 'xb')
+title('Dvi funkcijos')
+xlabel('X-ai')
+ylabel('F_1 [-o-]   |  F_2 [-x]')
