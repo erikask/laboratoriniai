d53c10c37236b5ca5b0a8fde1b6ed4c85e64e071
Erikas Kėvelaitis <erikas.kev@gmail.com>
2026-09-11 11:06:29 +0300

    Update LD1_ErikasKevelaitis.m

--- LD1/LD1_ErikasKevelaitis.m
+++ LD1/LD1_ErikasKevelaitis.m
@@ -13,3 +13,17 @@ plot(x, y, 'o-r', x, y/3, 'xb')
 title('Dvi funkcijos')
 xlabel('X-ai')
 ylabel('F_1 [-o-]   |  F_2 [-x]')
+
+%
+%Papildoma uzduotis
+% 
+
+N = 0;
+v = (N+1) : 0.5 : (N+4);
+A = reshape(N : (N+8), 3, 3)';
+A_a = A(3,2)
+A_b = A(2:3,1:2)
+A_c = A([1, 3],[1, 3])
+
+v_mod = v(1:3);
+A_mod = [A; v_mod]
